8c1df19f07c891e3d9b9da8af3ca3925ed1fe76b
ihundal8478 <ihundal8478@conestogac.on.ca>
2026-04-02 14:37:53 -0400

    Function for removing an item from the cart

--- Project Starter/Project/main.c
+++ Project Starter/Project/main.c
@@ -7,12 +7,14 @@ int addItemToCart(int currentItems, int itemsToAdd)
         return INVALID_VALUE;
     return currentItems + itemsToAdd;                      //Returns new total item count
 }
-
-
-
+    // Function to remove items from the cart
 int removeItemFromCart(int currentItems, int itemsToRemove) 
 {
-    return -7;
+        if (currentItems < 0 || itemsToRemove < 0)              // if inputs are negative then INVALID_VALUE (-1) is returned
+            return INVALID_VALUE;
+        if (itemsToRemove > currentItems)                       // if customer enters more items to remove than the items available in the cart then EMPTY_CART (0) is returned
+            return EMPTY_CART;
+        return currentItems - itemsToRemove;                    // returns new number of items present in the cart after removing items
 }
 
 double calculateTotalPrice(double itemPrice, int quantity, double taxRate) 
